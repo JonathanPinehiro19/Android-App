c6101adb9cea655215516c7a2b78d2ad5693a98b
JonathanPinheiro <jonathandevopssenac@gmail.com>
2023-06-28 19:41:44 -0300

    Revisão.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
+import { E404Component } from './pages/e404/e404.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Rota padrão → Página inicial → 'home'.
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    title: 'Página Inicial',
+    component: HomeComponent
+  },
+  {
+    path: 'contacts',
+    title: 'Faça Contato',
+    component: ContactsComponent
+  },
+  {
+    path: 'about',
+    title: 'Sobre',
+    component: AboutComponent
+  },
+  {
+    path: '404',
+    title: 'Erro 404',
+    component: E404Component
+  },
+
+  {
+    path: '**',
+    redirectTo: '404',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThemeComponent } from './theme/theme.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { E404Component } from './pages/e404/e404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThemeComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactsComponent,
+    E404Component
   ],
   imports: [
     BrowserModule,
